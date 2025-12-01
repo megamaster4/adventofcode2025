d07459119a56c0227bbc54d43be221ee026ffe97
megamaster4 <max.pot32@proton.me>
2025-12-01 20:56:55 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from adventofcode25!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ solutions/day1.py
@@ -0,0 +1,7 @@
+def main():
+    dial = list(range(0,100))
+    start = dial[50]
+    print(start)
+
+if __name__ == "__main__":
+    main()
